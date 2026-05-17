d9210b55c8979caac314764c5f6c874eec67bbb5
Pabasara Weerasooriya <pdewmini2002@gmail.com>
2026-05-17 12:28:07 +0530

    Added app.jsx, index.css and main.jsx files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Navbar from "./components/navbar/navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Contact from "./pages/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
+function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className="app">
+        <Navbar
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          setShowLogin={setShowLogin}
+        />
+        <Routes>
+          <Route path="/" element={<Home searchQuery={searchQuery} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
